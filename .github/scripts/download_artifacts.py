4b0878f74da233659926bb27c199a2a92d95fc2b
ivsk <ivakevics.krisztian@gmail.com>
2025-06-14 19:38:18 +0200

    update inference build

---
+++ .github/scripts/download_artifacts.py
@@ -0,0 +1,45 @@
+import mlflow
+import sagemaker
+import boto3
+import os
+import logging
+import argparse
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("mlflow_download_artifacts")
+
+
+def setup_mlflow():
+    boto_session = boto3.Session()
+    sagemaker_session = sagemaker.Session(boto_session=boto_session)
+    sagemaker_client = boto_session.client(service_name="sagemaker")
+    mlflow.set_tracking_uri(os.environ.get("MLFLOW_TRACKING_SERVER_ARN"))
+
+
+def download_artifacts(model_name: str) -> None:
+    client = mlflow.MlflowClient()
+
+    latest_model_version = client.get_latest_version(model_name)[0]
+    client.download_artifacts(
+        run_id=latest_model_version.run_id, path=model_name, dst_path=os.getcwd()
+    )
+
+    logger.info(f"Artifacts downloaded with run ID: {latest_model_version.run_id}")
+    logger.info(f"Contents of current directory: {os.listdir()}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="MLFlow artifact downloader")
+
+    parser.add_argument(
+        "--model_name",
+        required=True,
+        help="Model name associated with the artifacts to download.",
+    )
+
+    args = parser.parse_args()
+    download_artifacts(args.model_name)
+
+
+if __name__ == "__main__":
+    main()
